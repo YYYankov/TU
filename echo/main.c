07a8a308a4a7160b8d0e6f73438b6ba1e706e357
YYYankov <96125323+YYYankov@users.noreply.github.com>
2022-07-04 22:16:45 +0300

    Create main.c

---
+++ echo/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;
+    while(( c=getchar() ) != EOF){ /* EOF = sds */
+            putchar(c);
+
+    }
+
+
+
+
+    //printf("Hello world!\n");
+    return 0;
+}
