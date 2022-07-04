315af862e7f0ef7ce19290ac3349efd16ce57d8c
YYYankov <96125323+YYYankov@users.noreply.github.com>
2022-07-04 22:17:14 +0300

    Create main.c

---
+++ swapvalues/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=10, b=20;
+    a=a*b;
+    b=a/b;
+    a=a/b;
+    printf("After swap a=%d b=%d",a,b);
+
+
+    //printf("Hello world!\n");
+    return 0;
+}
